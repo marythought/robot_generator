6de94a6746a36436361210e49bbb713e62077f7e
Mary Dickson <mdickson@gmail.com>
2015-07-20 19:31:56 -0700

    no negative robot arms

--- app/models/robot_list.rb
+++ app/models/robot_list.rb
@@ -1,4 +1,7 @@
 class RobotList < ActiveRecord::Base
   validates :name, presence: true
-  validates :arms,
+  validates_numericality_of :arms, :only_integer =>true,
+                            :allow_blank => true,
+                            :greater_than_or_equal_to =>0,
+                            :message => "can't be negative, sorry"
 end
